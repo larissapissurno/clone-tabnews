7892212da627c80d788ad15dee43d0581e13454f
Larissa Pissurno <larissapissurno@gmail.com>
2025-03-09 13:12:39 -0300

    add first page

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return <div style={styles.container}>Cloning tabnews! 🚀</div>;
+}
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+
+    fontFamily: "sans-serif",
+    fontSize: 24,
+  },
+};
